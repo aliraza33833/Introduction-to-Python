5c94f3aa65b0888a3ba434fb2ae4c552b850d17c
Ali Raza <aliraza33833@gmail.com>
2021-11-01 16:14:08 +0500

    Update 2.0

--- 03_Functions and Packages.py
+++ 03_Functions and Packages.py
@@ -1,102 +1,53 @@
 # 01_Functions
-# Create variables var1 and var2
 var1 = [1, 2, 3, 4]
 var2 = True
-
-# Print out type of var1
 print(type(var1))
-
-# Print out length of var1
 print(len(var1))
 
-# Convert var2 to an integer: out2
-out2 = int(var2)
+out2 = int(var2) # Convert var2 to an integer
 
-
-# Create lists first and second
 first = [11.25, 18.0, 20.0]
 second = [10.75, 9.50]
-
-# Paste together first and second: full
 full = first + second
 
-# Sort full in descending order: full_sorted
-full_sorted = sorted(full, reverse=True)
-
-# Print out full_sorted
+full_sorted = sorted(full, reverse=True) # Sort full in descending order
 print(full_sorted)
 
 
 # 02_Methods
-# string to experiment with: place
 place = "poolhouse"
-
-# Use upper() on place: room_up
 place_up = place.upper()
-
-# Print out place and room_up
 print(place)
 print(place_up)
+print(place.count("o")) # Print out the number of o's in place
 
-# Print out the number of o's in place
-print(place.count("o"))
-
-
-# Create list areas
 areas = [11.25, 18.0, 20.0, 10.75, 9.50]
-
-# Print out the index of the element 20.0
-print(areas.index(20.0))
-
-# Print out how often 14.5 appears in areas
-print(areas.count(9.50))
+print(areas.index(20.0)) # Print out the index of the element 20.0
+print(areas.count(9.50)) # Print out how often 9.5 appears in areas
 print(areas)
 
-
-# Create list areas
-areas = [11.25, 18.0, 20.0, 10.75, 9.50]
-
-# Use append twice to add poolhouse and garage size
-areas.append(24.5)
+areas.append(24.5) # Use append twice to add poolhouse and garage size
 areas.append(15.45)
-
-
-# Print out areas
 print(areas)
 
-# Reverse the orders of the elements in areas
-areas.reverse()
-
-# Print out areas
+areas.reverse() # Reverse the orders of the elements in areas
 print(areas)
 
 
 # 03_Package
-# Definition of radius
-r = 0.43
+r = 0.43 # Definition of radius
 
-# Import the math package
-import math
+import math # Import the math package
 
-# Calculate C
-C = 2*math.pi*r
-
-# Calculate A
-A = math.pi*r**2
-
-# Build printout
+C = 2*math.pi*r # Calculate C
+A = math.pi*r**2 # Calculate A
 print("Circumference: " + str(C))
 print("Area: " + str(A))
 
-
-# Definition of radius
-r = 192500
+r = 192500 # Definition of radius
 phi = 12
-# Import radians function of math package
-from math import radians
 
-# Travel distance of Moon over 12 degrees. Store in dist.
-dist = radians(12)*r
+from math import radians # Import radians function of math package
 
-# Print out dist
+dist = radians(12)*r # Travel distance of Moon over 12 degrees. Store in dist.
 print(dist)
